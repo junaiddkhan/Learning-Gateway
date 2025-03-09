8fed07f68a4688d864eacf54925bed311f756434
Junaid Khan <junaidkhan75544@gmail.com>
2025-03-09 23:27:57 +0500

    first commit

---
+++ assets/js/courses.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf01011ae0328fe4c906db436929fb59c9987f1675e1064ad5a9fd5290eac233
+size 710
---
+++ assets/js/login.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f76b4c13ad953192c8aad29f17246dcb34955eef3658474e1202d5e98f4acf6
+size 378
